3f5597d4f076a615d47025fec283897f1d6c2297
snovakow <scott.novakowski@deep-inc.com>
2026-02-22 11:34:57 -0500

    Create data.ts

---
+++ src/data.ts
@@ -0,0 +1,135 @@
+import type { Team } from "./logo";
+
+type DataString = "firstName" | "lastName";
+type RowDataString = Record<DataString, string>;
+
+export type KeyNumber = "gg" | "bet1" | "bet2" | "bet3" | "bet4";
+export type RowKeyNumber = Record<KeyNumber, number>;
+
+export type RowData = RowDataString & RowKeyNumber & { team: Team };
+
+export const table1Data: RowData[] = [
+    {
+        firstName: "Anze",
+        lastName: "Kopitar",
+        team: "LAK",
+        gg: 0.2,
+        bet1: 200,
+        bet2: 220,
+        bet3: 200,
+        bet4: 200
+    }, {
+        firstName: "Reilly",
+        lastName: "Smith",
+        team: "VGK",
+        gg: 0.21,
+        bet1: 210,
+        bet2: 220,
+        bet3: 200,
+        bet4: 200
+    }, {
+        firstName: "Taylor",
+        lastName: "Hall",
+        team: "CAR",
+        gg: 0.21,
+        bet1: 220,
+        bet2: 200,
+        bet3: 220,
+        bet4: 200
+    }, {
+        firstName: "Mikael",
+        lastName: "Granlund",
+        team: "ANA",
+        gg: 0.2,
+        bet1: 230,
+        bet2: 200,
+        bet3: 220,
+        bet4: 200
+    },
+];
+
+export const table2Data: RowData[] = [
+    {
+        firstName: "Ryan",
+        lastName: "Strome",
+        team: "ANA",
+        gg: 0.2,
+        bet1: 200,
+        bet2: 220,
+        bet3: 200,
+        bet4: 200
+    },
+    {
+        firstName: "Hampus",
+        lastName: "Lindholm",
+        team: "BOS",
+        gg: 0.21,
+        bet1: 210,
+        bet2: 220,
+        bet3: 200,
+        bet4: 200
+    },
+    {
+        firstName: "Jordan",
+        lastName: "Martinook",
+        team: "CAR",
+        gg: 0.21,
+        bet1: 220,
+        bet2: 200,
+        bet3: 220,
+        bet4: 200
+    },
+    {
+        firstName: "Nicholas",
+        lastName: "Paul",
+        team: "TBL",
+        gg: 0.2,
+        bet1: 230,
+        bet2: 200,
+        bet3: 220,
+        bet4: 200
+    },
+];
+
+export const table3Data: RowData[] = [
+    {
+        firstName: "Ryan",
+        lastName: "McDonagh",
+        team: "TBL",
+        gg: 0.2,
+        bet1: 200,
+        bet2: 220,
+        bet3: 200,
+        bet4: 200
+    },
+    {
+        firstName: "Yanni",
+        lastName: "Gourde",
+        team: "TBL",
+        gg: 0.21,
+        bet1: 210,
+        bet2: 220,
+        bet3: 200,
+        bet4: 200
+    },
+    {
+        firstName: "Jaccob",
+        lastName: "Slavin",
+        team: "CAR",
+        gg: 0.21,
+        bet1: 220,
+        bet2: 200,
+        bet3: 220,
+        bet4: 200
+    },
+    {
+        firstName: "Ross",
+        lastName: "Johnston",
+        team: "ANA",
+        gg: 0.2,
+        bet1: 230,
+        bet2: 200,
+        bet3: 220,
+        bet4: 200
+    },
+];
