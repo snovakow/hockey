e05c8ee4f5eb7131556ff681bf0cbbff8283212d
snovakow <snovakow@gmail.com>
2026-02-22 11:38:01 -0500

    Update App.tsx
    Update table sorting method and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { table1Data, table2Data, table3Data } from './data';
+import type { KeyNumber, RowKeyNumber, RowData } from './data';
+import { getLogo } from './logo';
 
 type KeyString = "name";
-type KeyNumber = "gg" | "bet1" | "bet2" | "bet3" | "bet4";
+type RowKeyString = Record<KeyString, string>;
+
 type KeyType = KeyString | KeyNumber;
 
-type RowDataString = Record<KeyString, string>;
-type RowDataNumber = Record<KeyNumber, number>;
-
-type RowData = RowDataString & RowDataNumber;
+type RowKey = RowKeyString & RowKeyNumber & Record<"rawOrder", number>;
+type OrderKeyType = KeyType | "rawOrder";
 
 interface ColumnData {
   key: KeyType;
@@ -18,7 +20,7 @@ interface ColumnData {
 }
 
 const columns: ColumnData[] = [
-  { key: "name", title: "Name" },
+  { key: "name", title: "Player" },
   { key: "gg", title: "GG" },
   { key: "bet1", title: "Bet1" },
   { key: "bet2", title: "Bet2" },
@@ -26,32 +28,43 @@ const columns: ColumnData[] = [
   { key: "bet4", title: "Bet4" },
 ];
 
-const table1Rows: RowData[] = [
-  { name: "Anze Kopitar", gg: 0.2, bet1: 200, bet2: 220, bet3: 200, bet4: 200 },
-  { name: "Reilly Smith", gg: 0.21, bet1: 210, bet2: 220, bet3: 200, bet4: 200 },
-  { name: "Taylor Hall", gg: 0.21, bet1: 220, bet2: 200, bet3: 220, bet4: 200 },
-  { name: "Mikael Granlund", gg: 0.2, bet1: 230, bet2: 200, bet3: 220, bet4: 200 },
-];
-const table2Rows: RowData[] = [
-  { name: "Ryan Strome", gg: 0.2, bet1: 200, bet2: 220, bet3: 200, bet4: 200 },
-  { name: "Hampus Lindholm", gg: 0.21, bet1: 210, bet2: 220, bet3: 200, bet4: 200 },
-  { name: "Jordan Martinook", gg: 0.21, bet1: 220, bet2: 200, bet3: 220, bet4: 200 },
-  { name: "Nicholas Paul", gg: 0.2, bet1: 230, bet2: 200, bet3: 220, bet4: 200 },
-];
-const table3Rows: RowData[] = [
-  { name: "Ryan McDonagh", gg: 0.2, bet1: 200, bet2: 220, bet3: 200, bet4: 200 },
-  { name: "Yanni Gourde", gg: 0.21, bet1: 210, bet2: 220, bet3: 200, bet4: 200 },
-  { name: "Jaccob Slavin", gg: 0.21, bet1: 220, bet2: 200, bet3: 220, bet4: 200 },
-  { name: "Ross Johnston", gg: 0.2, bet1: 230, bet2: 200, bet3: 220, bet4: 200 },
-];
+const makeRows = (data: RowData[]): RowKey[] => {
+  return data.map((item, index) => {
+    return {
+      rawOrder: index,
+      name: `${item.lastName}, ${item.firstName}`,
+      gg: item.gg,
+      bet1: item.bet1,
+      bet2: item.bet2,
+      bet3: item.bet3,
+      bet4: item.bet4,
+    }
+  });
+}
 
-const getLastName = (fullName: string) => {
-  const firstSpaceIndex = fullName.indexOf(' ');
+const table1Rows = makeRows(table1Data);
+const table2Rows = makeRows(table2Data);
+const table3Rows = makeRows(table3Data);
 
-  if (firstSpaceIndex === -1) {
-    return fullName; // No space found, return the original string
-  } else {
-    return fullName.slice(firstSpaceIndex + 1);
+interface SortConfig {
+  keyOrder: KeyType[];
+}
+
+const sortFunction = (sortConfig: SortConfig) => {
+  return (a: RowKey, b: RowKey) => {
+    const keyOrder: OrderKeyType[] = [...sortConfig.keyOrder, "rawOrder"];
+    for (const key of keyOrder) {
+      const aVal = a[key];
+      const bVal = b[key];
+      if (typeof aVal === 'number' && typeof bVal === 'number') {
+        if (aVal !== bVal) return aVal - bVal;
+      }
+      if (typeof aVal === 'string' && typeof bVal === 'string') {
+        const comparison = aVal.localeCompare(bVal, undefined, { sensitivity: 'base' });
+        if (comparison !== 0) return comparison;
+      }
+    }
+    return 0;
   }
 }
 
@@ -66,30 +79,67 @@ function App() {
   const [rows3, setRows3] = useState(table3Rows);
   const sortedRows3 = [...rows3];
 
-  const [sortConfig, setSortConfig] = useState<{ key: KeyType; direction: 'asc' | 'desc'; } | null>(null);
+  const [sortConfig1, setSortConfig1] = useState<SortConfig | null>(null);
+  const [sortConfig2, setSortConfig2] = useState<SortConfig | null>(null);
+  const [sortConfig3, setSortConfig3] = useState<SortConfig | null>(null);
 
-  if (sortConfig !== null) {
-    sortedRows1.sort((a, b) => {
-      const aVal = a[sortConfig.key];
-      const bVal = b[sortConfig.key];
-      if (typeof aVal === 'number' && typeof bVal === 'number') {
-        return sortConfig.direction === 'asc' ? aVal - bVal : bVal - aVal;
-      }
-      if (typeof aVal === 'string' && typeof bVal === 'string') {
-        const aName = getLastName(aVal);
-        const bName = getLastName(bVal);
-        return aName.localeCompare(bName, undefined, { sensitivity: 'base' });
-      }
-      return 0;
-    });
+  if (sortConfig1 !== null) {
+    sortedRows1.sort(sortFunction(sortConfig1));
+  }
+  if (sortConfig2 !== null) {
+    sortedRows2.sort(sortFunction(sortConfig2));
+  }
+  if (sortConfig3 !== null) {
+    sortedRows3.sort(sortFunction(sortConfig3));
   }
 
-  const requestSort = (key: KeyType) => {
-    let direction: 'asc' | 'desc' = 'asc';
-    if (sortConfig && sortConfig.key === key && sortConfig.direction === 'asc') {
-      direction = 'desc';
+  const requestSort1 = (keyPrimary: KeyType) => {
+    if (!sortConfig1) {
+      setSortConfig1({ keyOrder: [keyPrimary] });
+      return;
     }
-    setSortConfig({ key, direction });
+    if (sortConfig1.keyOrder[0] === keyPrimary) {
+      setSortConfig1(null);
+      return;
+    }
+    const keyOrder = [keyPrimary];
+    for (const key of sortConfig1.keyOrder) {
+      if (key === keyPrimary) continue;
+      keyOrder.push(key);
+    }
+    setSortConfig1({ keyOrder });
+  };
+  const requestSort2 = (keyPrimary: KeyType) => {
+    if (!sortConfig2) {
+      setSortConfig2({ keyOrder: [keyPrimary] });
+      return;
+    }
+    if (sortConfig2.keyOrder[0] === keyPrimary) {
+      setSortConfig2(null);
+      return;
+    }
+    const keyOrder = [keyPrimary];
+    for (const key of sortConfig2.keyOrder) {
+      if (key === keyPrimary) continue;
+      keyOrder.push(key);
+    }
+    setSortConfig2({ keyOrder });
+  };
+  const requestSort3 = (keyPrimary: KeyType) => {
+    if (!sortConfig3) {
+      setSortConfig3({ keyOrder: [keyPrimary] });
+      return;
+    }
+    if (sortConfig3.keyOrder[0] === keyPrimary) {
+      setSortConfig3(null);
+      return;
+    }
+    const keyOrder = [keyPrimary];
+    for (const key of sortConfig3.keyOrder) {
+      if (key === keyPrimary) continue;
+      keyOrder.push(key);
+    }
+    setSortConfig3({ keyOrder });
   };
   const [count, setCount] = useState(0)
 
@@ -97,10 +147,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={getLogo("TOR", false)} className="logo" alt="Team logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={getLogo("TOR", true)} className="logo react" alt="Team logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -117,42 +167,44 @@ function App() {
       </p>
       {/* Sortable Table */}
       <div style={{ marginTop: 32 }}>
-        <h2>Pick # 1</h2>
+        <h2>Pick #1</h2>
         <table style={{ borderCollapse: 'collapse', width: '100%' }}>
           <thead>
             <tr>
               {
                 columns.map(item => (
                   <th key={item.key} style={{ cursor: 'pointer', border: '1px solid #ccc', padding: '8px' }}
-                    onClick={() => requestSort(item.key)}>
-                    {item.title} {sortConfig?.key === item.key ? (sortConfig.direction === 'asc' ? '▲' : '▼') : ''}
+                    onClick={() => requestSort1(item.key)}>
+                    {item.title} {sortConfig1?.keyOrder[0] === item.key ? '▲' : '△'}
                   </th>
                 ))
               }
             </tr>
           </thead>
           <tbody>
-            {sortedRows1.map((row, idx) => (
-              <tr key={idx}>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.name}</td>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.gg}</td>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet1}</td>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet2}</td>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet3}</td>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet4}</td>
-              </tr>
-            ))}
+            {
+              sortedRows1.map((row, idx) => (
+                <tr key={idx}>
+                  <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.name}</td>
+                  <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.gg.toFixed(2)}</td>
+                  <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet1}</td>
+                  <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet2}</td>
+                  <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet3}</td>
+                  <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet4}</td>
+                </tr>
+              ))
+            }
           </tbody>
         </table>
-        <h2>Pick # 2</h2>
+        <h2>Pick #2</h2>
         <table style={{ borderCollapse: 'collapse', width: '100%' }}>
           <thead>
             <tr>
               {
                 columns.map(item => (
                   <th key={item.key} style={{ cursor: 'pointer', border: '1px solid #ccc', padding: '8px' }}
-                    onClick={() => requestSort(item.key)}>
-                    {item.title} {sortConfig?.key === item.key ? (sortConfig.direction === 'asc' ? '▲' : '▼') : ''}
+                    onClick={() => requestSort2(item.key)}>
+                    {item.title} {sortConfig2?.keyOrder[0] === item.key ? '▲' : '△'}
                   </th>
                 ))
               }
@@ -162,7 +214,7 @@ function App() {
             {sortedRows2.map((row, idx) => (
               <tr key={idx}>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.name}</td>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.gg}</td>
+                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.gg.toFixed(2)}</td>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet1}</td>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet2}</td>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet3}</td>
@@ -171,15 +223,15 @@ function App() {
             ))}
           </tbody>
         </table>
-        <h2>Pick # 3</h2>
+        <h2>Pick #3</h2>
         <table style={{ borderCollapse: 'collapse', width: '100%' }}>
           <thead>
             <tr>
               {
                 columns.map(item => (
                   <th key={item.key} style={{ cursor: 'pointer', border: '1px solid #ccc', padding: '8px' }}
-                    onClick={() => requestSort(item.key)}>
-                    {item.title} {sortConfig?.key === item.key ? (sortConfig.direction === 'asc' ? '▲' : '▼') : ''}
+                    onClick={() => requestSort3(item.key)}>
+                    {item.title} {sortConfig3?.keyOrder[0] === item.key ? '▲' : '△'}
                   </th>
                 ))
               }
@@ -189,7 +241,7 @@ function App() {
             {sortedRows3.map((row, idx) => (
               <tr key={idx}>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.name}</td>
-                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.gg}</td>
+                <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.gg.toFixed(2)}</td>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet1}</td>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet2}</td>
                 <td style={{ border: '1px solid #ccc', padding: '8px' }}>{row.bet3}</td>
