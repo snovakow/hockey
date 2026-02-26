f29a893679ce663923a6b877e981006bb52b821e
snovakow <snovakow@gmail.com>
2026-02-26 12:39:59 -0500

    Cleanup
    Remove original template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import tsLogo from './assets/ts.png'
 import './App.css'
 import { table1Data, table2Data, table3Data } from './data';
 import { getLogo } from './components/logo';
@@ -191,30 +188,6 @@ function App() {
         Tims Hockey Challenge Picks
       </header>
       <main className='content'>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Team logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="Team logo" />
-          </a>
-          <a href="https://www.typescriptlang.org" target="_blank">
-            <img src={tsLogo} className="logo react" alt="Team logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-        {/* Sortable Table */}
         <div className="table-container">
           <h2>Pick #1</h2>
           <Table columns={columns} sortedRows={sortedRows1} requestSort={requestSort1} sortConfig={sortConfig1} darkTheme={darkTheme} chances={chances} />
